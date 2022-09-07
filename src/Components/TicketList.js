a4c9078c517dce54498941c25224dcd7b8bfa49d
ftolentino <filmer.tolentino@gmail.com>
2022-09-07 09:56:54 -0700

    Finish passing data via call backs lesson

--- src/Components/TicketList.js
+++ src/Components/TicketList.js
@@ -1,36 +1,23 @@
 import React from "react";
 import Ticket from "./Ticket.js";
+import PropTypes from "prop-types";
 
-const mainTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Prop types are throwing an error.'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Child component isn\'t rendering.'
-  }
-];
-
-const TicketList = () => {
+const TicketList = (props) => {
   return (
     <React.Fragment>
       <hr />
-      {mainTicketList.map((ticket, index) =>
-        <Ticket
-          names={ticket.names}
-          issue={ticket.location}
-          key={index} />
+      {props.ticketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+        location={ticket.location}
+        issue={ticket.issue}
+        key={index} />
       )}
     </React.Fragment>
   );
 }
+
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
